05e3e7db61edf6f7462428e0f240de9c89167def
Kokoa_Chino <2178740980@qq.com>
2024-08-22 19:54:58 +0800

    [1.0] Initial

---
+++ src/main/java/org/example/FileRename.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.io.File;
+import java.util.Objects;
+
+
+public class FileRename {
+    public static void run(File folder) { // 将文件夹内的所有文件重命名
+        for (File f: Objects.requireNonNull(folder.listFiles(File::isDirectory))) run(f);
+        File[] files = folder.listFiles(f -> {
+            if (f.isFile() && f.getName().lastIndexOf('.') >= 0) {
+                return !f.getName().substring(f.getName().lastIndexOf('.')).equals(".ini");
+            }
+            return false;
+        });
+        if (files == null) return;
+        int n = files.length;
+        int m = String.valueOf(n).length();
+        for (int i = 0; i < n; i++) {
+            File f = files[i];
+            String newName = folder.getName() + " " + String.format("%0" + m + "d", i + 1);
+            String type = f.getName().substring(f.getName().lastIndexOf('.'));
+            f.renameTo(new File(f.getParent() + "\\" + newName + type));
+        }
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.io.File;
+
+
+public class Main {
+    public static void main(String[] args) {
+        FileRename.run(new File("D:\\手机传输\\图片\\番剧\\请问您今天要来点兔子吗\\点兔自截图"));
+    }
+}
